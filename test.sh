1e4caa16904b951d2f7bbdb40e1c0db13cdea2c2
Spencer Claxton <saclaxton@gmail.com>
2013-10-03 15:54:27 -0500

    syncing

---
+++ test.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+VIMVER=7.3
+parse_version() {
+    str=$1
+    res=''
+    for (( i = 0; i < ${#1}; ++i)); do
+        curr=${str:i:1}
+        if [ $curr == '.' ]
+        then res=$res'\.'
+        else res=$res$curr
+        fi
+    done
+    echo $res
+}
+brew_version() {
+    regex=`parse_version $2`
+    formula=`brew versions $1 | grep ${regex}' '`
+    len=${#2}
+    formula=`echo ${formula:len} | sed -e 's/^[ \t]*//'`
+    echo $formula
+    (cd `brew --prefix`; brew uninstall $1; ${formula}; brew install $1; brew unlink $1 && brew link --overwrite $1)
+}
+
+brew_version vim $VIMVER
+hi\nhi
+hi\nhi
+hi hi
+n
+\n
+
+
+test
+test
